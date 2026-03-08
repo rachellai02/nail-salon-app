78db1d8120d5699aa0c239f40d1cbc815afb6f52
rachellai02 <lairachel02@gmail.com>
2026-03-08 23:22:33 +0800

    [fix] Make appointments tab landing tab

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,14 +31,14 @@ export default function RootLayout({
       >
         <nav className="bg-white border-b border-gray-200 px-6 py-4 flex items-center gap-6">
           <span className="font-bold text-lg" style={{fontSize: "20px"}}>Prestige <i style={{ fontSize: "14px"}}>by Chusen</i></span>
+          <Link href="/appointments" className="text-sm text-black-600 hover:text-gray-500 hover:font-bold hover:underline font-medium">
+            Appointments
+          </Link>
           <Link href="/packages" className="text-sm text-black-600 hover:text-gray-500 hover:font-bold hover:underline font-medium">
             Package Types
           </Link>
           <Link href="/packages/customers" className="text-sm text-black-600 hover:text-gray-500 hover:font-bold hover:underline font-medium">
             Customer's Packages
-          </Link>
-          <Link href="/appointments" className="text-sm text-black-600 hover:text-gray-500 hover:font-bold hover:underline font-medium">
-            Appointments
           </Link>
           <Link href="/packages/archive" className="text-sm text-black-600 hover:text-gray-500 hover:font-bold hover:underline font-medium">
             Archive
